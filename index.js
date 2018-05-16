9583fc32cd2e045114758028e0452ddbef3973d0
Sudhir Menon <sudhirhpp@gmail.com>
2018-05-16 15:51:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,5 @@ let randomNumber = Math.floor(Math.random()*21);
 
 let randomInteger = Math.round(randomNumber);
 
-let randomUserID = (randomInteger).isInteger();
+
+let randomUserID = randomInteger;
