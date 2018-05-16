88ed9996455fff03900e9f22c47104a47acda2cb
Sudhir Menon <sudhirhpp@gmail.com>
2018-05-16 15:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,4 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-oldID = 100;
-function newID{
-  return (1000000000 + oldID);
-}
+newID = 1000000000 +oldID;
